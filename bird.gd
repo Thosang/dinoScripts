9239fab403f7be23d48256bc67c1f167b078ad83
Thosang <thosang.014@gmail.com>
2025-09-04 15:42:04 +0700

    Update bird.gd

--- bird.gd
+++ bird.gd
@@ -4,4 +4,5 @@ func _ready() -> void:
 	pass # Replace with function body.
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	$AnimatedSprite2D.play("fly")
 	position.x -= get_parent().speed/2
